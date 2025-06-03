f963e02d38d736f3c38725cec99149b3f5d7dc37
Mykhailo Voznesenskyi <mykhailo.voznesenskyi@amedia.ua>
2025-06-03 11:48:01 +0300

    removed app service and controller

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from 'src/user/user.module';
+import { PostModule } from 'src/post/post.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, PostModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
